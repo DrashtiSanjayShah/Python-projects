8e4c5ced0f1ca64b36899b2ce9539b7001941365
Drashti Sanjay Shah <drashtisit120@gst.sies.edu.in>
2022-01-22 20:46:32 +0530

    first commit

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+# youtuber ="Drashti Shah "
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {} ".format (youtuber))
+# print(f"Subscribe to {youtuber}")
+ad = input("Adjective:")
+verb1 = input("Verb:")
+verb2 = input("Verb:")
+famous_person = input("Famous person:")
+madlib =f"Computer programming is so {ad}! It makes me so excited because  I love to {verb1}.  Stay hydrated and {verb2} like you are {famous_person}!"
+print(madlib)
