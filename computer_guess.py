be4207fbc5495b3f7721ba8e77c85843bbf8ac9e
DrashtiS <drashitsanjayshah@gmial.com>
2022-02-03 20:56:46 +0530

    computer guesses your number

---
+++ computer_guess.py
@@ -0,0 +1,21 @@
+import random
+
+def computer_guess(x):
+    low = 1
+    higher = x
+    feedback = ''
+    while feedback != 'c':
+        if low != higher:
+            guess = random.randint(low, higher)
+        else:
+            guess = low
+        guess = random.randint(low,higher)
+        feedback = input(f"Is {guess} too high (H), too low(L), or correct (C)??").lower()
+        if feedback == 'h':
+            higher = guess - 1
+            if feedback == "l":
+               l = guess + 1
+    print("YAYYYY! The computer guessed your number " + str(guess) + " correctly!!")
+               
+               
+computer_guess(100)
